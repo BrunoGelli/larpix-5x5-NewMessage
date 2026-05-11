704d0894df44c248e285ac70d861bfeb2db92e64
BrunoGelli <gelli@ifi.unicamp.br>
2026-05-11 16:00:18 -0700

    cleaning the code base

--- network_5x5.py
+++ network_5x5.py
@@ -7,10 +7,6 @@ from util import save_controller
 import pickle
 import tqdm
 
-def set_register(c, chip_key, register, value):
-    setattr(c[chip_key].config, register, value)
-    c.write_configuration(chip_key, register)
-
 #cm is the father, ck the current chip 
 def conf_east(c, cm, ck, cadd, iog, iochan):
     I_TX_DIFF = 7
@@ -159,127 +155,6 @@ def conf_south(c, cm, ck, cadd, iog, iochan):
     else:
         print(ck, 'unable to configure')
 
-def read(c, key, param):
-    c.reads = []
-    c.read_configuration(key, param, timeout=0.1)
-    message = c.reads[-1]
-    for msg in message:
-        if not isinstance(msg, larpix.packet.packet_v2.Packet_v2):
-            continue
-        if msg.packet_type not in [larpix.packet.packet_v2.Packet_v2.CONFIG_READ_PACKET]:
-            continue
-        print(msg)
-        # return msg.chip_id
-    return 0
-
-def enable_pedestal(c, key, vref_dac=255):
-
-    c[key].config.enable_external_sync = 1
-    c.write_configuration(key, 'enable_external_sync')
-    c[key].config.mark_first_packet = 0
-    c.write_configuration(key, 'mark_first_packet')
-    c[key].config.enable_periodic_trigger = 1
-    c[key].config.enable_rolling_periodic_trigger = 1
-    c[key].config.enable_periodic_reset = 1
-    c[key].config.enable_rolling_periodic_reset = 1
-    c[key].config.enable_hit_veto = 1
-    c[key].config.enable_periodic_trigger_veto = 0
-
-    c[key].config.threshold_global = 255
-    c[key].config.periodic_trigger_cycles = 57812
-    c[key].config.periodic_reset_cycles = 40
-
-    c[key].config.cds_mode = 0
-    c[key].config.enable_data_stats = 0
-    c[key].config.vref_dac = vref_dac
-
-    c[key].config.ibias_vcm_buffer = 15
-    c.write_configuration(key, 'ibias_vcm_buffer')
-
-    c[key].config.adc_comp_trim = 2
-    c.write_configuration(key, 'adc_comp_trim')
-
-    c[key].config.adc_ibias_delay = 7
-    c.write_configuration(key, 'adc_ibias_delay')
-
-    c.write_configuration(key, 'vref_dac')
-
-    c.write_configuration(key, 'enable_data_stats')
-    c.write_configuration(key, 'enable_periodic_trigger')
-    c.write_configuration(key, 'cds_mode')
-    c.write_configuration(key, 'enable_rolling_periodic_trigger')
-    c.write_configuration(key, 'enable_periodic_reset')
-    c.write_configuration(key, 'enable_rolling_periodic_reset')
-    c.write_configuration(key, 'enable_hit_veto')
-    c.write_configuration(key, 'enable_periodic_trigger_veto')
-    c.write_configuration(key, 'threshold_global')
-    c.write_configuration(key, 'periodic_trigger_cycles')
-
-    c.write_configuration(key, 'periodic_reset_cycles')
-
-    ok, diff = c.enforce_configuration(key, n=3, n_verify=3, timeout=0.1)
-    print(key, ' pedestal enabled:', ok)
-    if not ok:
-        print(diff)
-
-def enable_SelfTrigger(c, key, vref_dac=255):
-
-    c[key].config.enable_external_sync = 1
-    c.write_configuration(key, 'enable_external_sync')
-    c[key].config.mark_first_packet = 0
-    c.write_configuration(key, 'mark_first_packet')
-    c[key].config.enable_periodic_trigger = 0
-    c[key].config.enable_rolling_periodic_trigger = 0
-    c[key].config.enable_periodic_reset = 1
-    c[key].config.enable_rolling_periodic_reset = 1
-    c[key].config.enable_hit_veto = 1
-    c[key].config.enable_periodic_trigger_veto = 0
-
-    c[key].config.threshold_global = 255
-    c[key].config.periodic_reset_cycles = 40
-
-    c[key].config.cds_mode = 0
-    c[key].config.enable_data_stats = 0
-    c[key].config.vref_dac = vref_dac
-
-    c[key].config.ibias_vcm_buffer = 15
-    c.write_configuration(key, 'ibias_vcm_buffer')
-
-    c[key].config.adc_comp_trim = 2
-    c.write_configuration(key, 'adc_comp_trim')
-
-    c[key].config.adc_ibias_delay = 7
-    c.write_configuration(key, 'adc_ibias_delay')
-
-    c.write_configuration(key, 'vref_dac')
-
-    c.write_configuration(key, 'enable_data_stats')
-    c.write_configuration(key, 'enable_periodic_trigger')
-    c.write_configuration(key, 'cds_mode')
-    c.write_configuration(key, 'enable_rolling_periodic_trigger')
-    c.write_configuration(key, 'enable_periodic_reset')
-    c.write_configuration(key, 'enable_rolling_periodic_reset')
-    c.write_configuration(key, 'enable_hit_veto')
-    c.write_configuration(key, 'enable_periodic_trigger_veto')
-    c.write_configuration(key, 'threshold_global')
-
-    c.write_configuration(key, 'periodic_reset_cycles')
-
-    ok, diff = c.enforce_configuration(key, n=3, n_verify=3, timeout=0.1)
-    print(key, ' pedestal enabled:', ok)
-    if not ok:
-        print(diff)
-
-def unmask(c, keys):
-    for key in reversed(keys):
-
-        c[key].config.csa_enable = [1]*64
-        c[key].config.channel_mask = [0]*64
-        c[key].config.periodic_trigger_mask = [1]*64
-        c.write_configuration(key, 'periodic_trigger_mask')
-        c.write_configuration(key, 'csa_enable')
-        c.write_configuration(key, 'channel_mask')
-
 def conf_root(c, cm, cadd, iog, iochan, pacman_version):
     I_TX_DIFF = 7
     TX_SLICE = 15
@@ -373,7 +248,7 @@ def main(vdda, vddd, verbose=True):
     pacman_version = 'v1rev5'
     pacman_tile = [PACMAN_TILE]
 
-      #Disable all UARTs -> just poke the register 0x001003f0 to disable all (f means all)
+    #Disable all UARTs -> just poke the register 0x001003f0 to disable all (f means all)
     c.io.set_reg(0x001003f0, 0, io_group)
 
 
@@ -515,18 +390,12 @@ def main(vdda, vddd, verbose=True):
     conf_east(c,chip54_key,chip55_key,55,IO_GROUP,IO_CHAN)
     all_keys.append(chip55_key)
 
-    # for ChipKey in all_keys:
-    #     # enable_pedestal(c, ChipKey,vref_dac=223)
-    #     enable_SelfTrigger(c, ChipKey,vref_dac=223)
-
-
-    # unmask(c, all_keys)
-
     # request internal reset at 0x00100410 and provided tile mask
     c.io.set_reg(0x00100410, 0x3ff, io_group)
 
+    print("Saving Controller")
     save_controller(c)
-    
+
     return
 
 if __name__ == '__main__':
