9f692eab663d97d4fe575a455f6a8852da7bfb40
BrunoGelli <gelli@ifi.unicamp.br>
2026-05-11 11:40:59 -0700

    testing self trigger

--- network_5x5.py
+++ network_5x5.py
@@ -19,7 +19,7 @@ def conf_east(c, cm, ck, cadd, iog, iochan):
     I_RX = 3
     V_CM = 5
 
-# add second chip
+    # add second chip
     # set mother transceivers
     c.add_chip(ck, version=3)
     c[cm].config.i_rx3 = I_RX
@@ -93,7 +93,7 @@ def conf_south(c, cm, ck, cadd, iog, iochan):
     I_RX = 3
     V_CM = 5
 
-# add second chip
+    # add second chip
     # set mother transceivers rx2, tx1
     c.add_chip(ck, version=3)
     c[cm].config.i_rx2 = I_RX
@@ -222,6 +222,54 @@ def enable_pedestal(c, key, vref_dac=255):
     if not ok:
         print(diff)
 
+def enable_SelfTrigger(c, key, vref_dac=255):
+
+    c[key].config.enable_external_sync = 1
+    c.write_configuration(key, 'enable_external_sync')
+    c[key].config.mark_first_packet = 0
+    c.write_configuration(key, 'mark_first_packet')
+    c[key].config.enable_periodic_trigger = 0
+    c[key].config.enable_rolling_periodic_trigger = 0
+    c[key].config.enable_periodic_reset = 1
+    c[key].config.enable_rolling_periodic_reset = 1
+    c[key].config.enable_hit_veto = 1
+    c[key].config.enable_periodic_trigger_veto = 0
+
+    c[key].config.threshold_global = 255
+    c[key].config.periodic_reset_cycles = 40
+
+    c[key].config.cds_mode = 0
+    c[key].config.enable_data_stats = 0
+    c[key].config.vref_dac = vref_dac
+
+    c[key].config.ibias_vcm_buffer = 15
+    c.write_configuration(key, 'ibias_vcm_buffer')
+
+    c[key].config.adc_comp_trim = 2
+    c.write_configuration(key, 'adc_comp_trim')
+
+    c[key].config.adc_ibias_delay = 7
+    c.write_configuration(key, 'adc_ibias_delay')
+
+    c.write_configuration(key, 'vref_dac')
+
+    c.write_configuration(key, 'enable_data_stats')
+    c.write_configuration(key, 'enable_periodic_trigger')
+    c.write_configuration(key, 'cds_mode')
+    c.write_configuration(key, 'enable_rolling_periodic_trigger')
+    c.write_configuration(key, 'enable_periodic_reset')
+    c.write_configuration(key, 'enable_rolling_periodic_reset')
+    c.write_configuration(key, 'enable_hit_veto')
+    c.write_configuration(key, 'enable_periodic_trigger_veto')
+    c.write_configuration(key, 'threshold_global')
+
+    c.write_configuration(key, 'periodic_reset_cycles')
+
+    ok, diff = c.enforce_configuration(key, n=3, n_verify=3, timeout=0.1)
+    print(key, ' pedestal enabled:', ok)
+    if not ok:
+        print(diff)
+
 def unmask(c, keys):
     for i in range(10):
         for key in reversed(keys):
@@ -232,7 +280,6 @@ def unmask(c, keys):
             c.write_configuration(key, 'periodic_trigger_mask')
             c.write_configuration(key, 'csa_enable')
             c.write_configuration(key, 'channel_mask')
-
 
 def conf_root(c, cm, cadd, iog, iochan, pacman_version):
     I_TX_DIFF = 7
@@ -470,44 +517,14 @@ def main(vdda, vddd, verbose=True):
     all_keys.append(chip55_key)
 
     for ChipKey in all_keys:
-        enable_pedestal(c, ChipKey,vref_dac=223)
+        # enable_pedestal(c, ChipKey,vref_dac=223)
+        enable_SelfTrigger(c, ChipKey,vref_dac=223)
         
-    # enable_pedestal(c, chip11_key, vref_dac=223)
-    # enable_pedestal(c, chip12_key, vref_dac=223)
-    # enable_pedestal(c, chip13_key, vref_dac=223)
-    # enable_pedestal(c, chip14_key, vref_dac=223)
-    # enable_pedestal(c, chip15_key, vref_dac=223)
 
     unmask(c, all_keys)
 
     # request internal reset at 0x00100410 and provided tile mask
     c.io.set_reg(0x00100410, 0x3ff, io_group)
-
-
-
-   
-    ## c.reads.clear()
-    # all_configured=True
-    # not_configured=[]
-    # for key in tqdm.tqdm(all_keys):
-
-    #     set_register(c, key, 'channel_mask', [0]*64)
-    #     set_register(c, key, 'csa_enable', [1]*64)
-    #     set_register(c, key, 'enable_periodic_trigger', 1)
-    #     set_register(c, key, 'periodic_trigger_cycles', 57812)
-
-    #     ok, diff = c.enforce_configuration(key, timeout=0.1, n=2, n_verify=2)
-    #     if ok:
-    #         pass
-    #     else:
-    #         all_configured=False
-    #         not_configured.append(key)
-
-    # if all_configured:
-    #     print('All chips configured!')
-    # else:
-    #     print('NOT configured:', not_configured)
-    # save_controller(c)
 
     return
 
